554aec3ce1ed8e2d8831a6cbabe0a72b5ecff627
Haythem Dridi <haithemdridiweb@gmail.com>
2023-03-28 16:12:40 +0100

    refactory contract

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Services\Fonts;
+use App\Services\ImageService;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
@@ -22,80 +24,30 @@ class ContractController extends Controller
             ->where('id', $request->order_id)
             ->firstOrFail();
 
-        $filename = '';
-
-        $fontsize = 14;
-        $fontfamily = Storage::disk('local')->path('fonts/ARIAL.TTF');
-        $fontfamilybold = Storage::disk('local')->path('fonts/ARIALBD.TTF');
 
         if (!$order->product->price_without_promo) {
-            $img = Image::make(Storage::disk('local')->path('contacts/default.png'));
+            $default = 'contacts/default.png';
         } else {
-            $img = Image::make(Storage::disk('local')->path('contacts/default_avec_promo.png'));
+            $default = 'contacts/default_avec_promo.png';
         }
 
-        if ($order->payment_by !== $order->doctor->FAMNAME . ' ' . $order->doctor->SHORTNAME) {
-            $img->text('OFFERTE PAR LES LABORATOIRES ' . $order->payment_by, 262, 85, function ($font) use ($fontsize, $fontfamily) {
-                $font->file($fontfamily);
-                $font->size($fontsize + 1);
-            });
-        }
+        $img = new ImageService(Storage::disk('local')->path($default));
 
+        $img->text('OFFERTE PAR LES LABORATOIRES ' . $order->payment_by, 262, 85, 15, Fonts::ARIAL, $order->payment_by !== $order->doctor->FAMNAME . ' ' . $order->doctor->SHORTNAME);
+        $img->text($order->doctor->FAMNAME . ' ' . $order->doctor->SHORTNAME, 112, 156, 14, Fonts::ARIAL);
+        $img->text($order->product->name, 112, 177, 14, Fonts::ARIAL);
+        $img->text($order->doctor->ADRESSE, 112, 199, 14, Fonts::ARIAL);
+        $img->text($order->doctor->GSM, 112, 220, 14, Fonts::ARIAL);
+        $img->text($order->date->format('d/m/Y'), 720, 158, 14, Fonts::ARIAL);
+        $img->text($order->licenses, 750, 178, 14, Fonts::ARIALBD);
+        $img->text($order->doctor->EMAIL, 580, 220, 14, Fonts::ARIAL);
+        $img->text(str_replace('Medwin', '', $order->product->name), 317, 532, 13, Fonts::ARIALBD);
+        $img->text($order->product->price_without_promo ? $order->product->price_without_promo : $order->product->price, 437, 532, 14, Fonts::ARIALBD);
+        $img->text($order->product->price, 425, 550, 14, Fonts::ARIALBD, $order->product->price_without_promo != null);
 
-        $img->text($order->doctor->FAMNAME . ' ' . $order->doctor->SHORTNAME, 112, 156, function ($font) use ($fontsize, $fontfamily) {
-            $font->file($fontfamily);
-            $font->size($fontsize);
-        });
-
-        $img->text($order->product->name, 112, 177, function ($font) use ($fontsize, $fontfamily) {
-            $font->file($fontfamily);
-            $font->size($fontsize);
-        });
-
-        $img->text($order->doctor->ADRESSE, 112, 199, function ($font) use ($fontsize, $fontfamily) {
-            $font->file($fontfamily);
-            $font->size($fontsize);
-        });
-
-        $img->text($order->doctor->GSM, 112, 220, function ($font) use ($fontsize, $fontfamily) {
-            $font->file($fontfamily);
-            $font->size($fontsize);
-        });
-
-        $img->text($order->date->format('d/m/Y'), 720, 158, function ($font) use ($fontsize, $fontfamily) {
-            $font->file($fontfamily);
-            $font->size($fontsize);
-        });
-
-        $img->text($order->licenses, 750, 178, function ($font) use ($fontsize, $fontfamilybold) {
-            $font->file($fontfamilybold);
-            $font->size($fontsize);
-        });
-
-        $img->text($order->doctor->EMAIL, 580, 220, function ($font) use ($fontsize, $fontfamily) {
-            $font->file($fontfamily);
-            $font->size($fontsize);
-        });
-
-        $img->text(str_replace('Medwin', '', $order->product->name), 317, 532, function ($font) use ($fontsize, $fontfamilybold) {
-            $font->file($fontfamilybold);
-            $font->size($fontsize - 1);
-        });
-
-        $img->text($order->product->price_without_promo ? $order->product->price_without_promo : $order->product->price, 437, 532, function ($font) use ($fontsize, $fontfamilybold) {
-            $font->file($fontfamilybold);
-            $font->size($fontsize);
-        });
-
-        if ($order->product->price_without_promo) {
-            $img->text($order->product->price, 425, 550, function ($font) use ($fontsize, $fontfamilybold) {
-                $font->file($fontfamilybold);
-                $font->size($fontsize);
-            });
-        }
 
         $filename = 'contacts/' . $order->id . '.jpeg';
-        $img->save(Storage::disk('local')->path($filename));
+        $img->save($filename);
 
         // return "<img src='storage/{$filename}'>";
         return Storage::disk('local')->download($filename, 'contrat' . $order->id . '.png');
