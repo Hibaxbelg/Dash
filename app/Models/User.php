c320b190ec3860cdff01006a711305be71cff4ec
Haythem Dridi <haithemdridiweb@gmail.com>
2023-05-13 21:53:44 +0100

    Fix Bug : delete user , don't delete his actions

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use App\Traits\StoreAction;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
@@ -14,7 +15,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, StoreAction;
+    use HasApiTokens, HasFactory, Notifiable, StoreAction, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
